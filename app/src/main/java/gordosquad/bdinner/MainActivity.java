4b3800def0d53088e9e2825cdd8ac2c09803eedd
paguiarmtz <paulaguiar29@gmail.com>
2017-05-19 01:50:19 -0500

    Inicio de app

--- app/src/main/java/gordosquad/bdinner/MainActivity.java
+++ app/src/main/java/gordosquad/bdinner/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "de version", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Aqui debe de ir la primera vista", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
